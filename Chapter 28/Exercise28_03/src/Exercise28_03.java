4970389325a054f68a3cb511be8f765f904164c2
JasonBeach11 <119906700+JasonBeach11@users.noreply.github.com>
2023-06-06 18:54:58 -0600

    Add files via upload

---
+++ Chapter 28/Exercise28_03/src/Exercise28_03.java
@@ -0,0 +1,53 @@
+/**
+ * @author Jason Beach
+ * @Date 05/30/2023
+ * CSCI 1175 Industry Projects
+ * Exercise 28-03: (Implement DFS using a stack) The depth-first search algorithm described in Listing 28.8 uses recursion. 
+ * Write a new method that performs a depth-first search without using recursion, called dfsNonRecurs().
+ * The UnweightedGraph file contains the algorithm for dfs without using recursion. 
+ * 
+ * Note: When using the List neighbors, be aware that it is a List within a List. In other words, to see how many neighbors 
+ * vertex x has, you would use neighbors.get(x). Then to actually grab the first neighbor vertex, you would use 
+ * neighbors.get(x).get(0).
+ * */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Exercise28_03 {
+  public static void main(String[] args) {
+    String[] vertices = {"Seattle", "San Francisco", "Los Angeles",
+      "Denver", "Kansas City", "Chicago", "Boston", "New York",
+      "Atlanta", "Miami", "Dallas", "Houston"};
+
+    int[][] edges = {
+      {0, 1}, {0, 3}, {0, 5},
+      {1, 0}, {1, 2}, {1, 3},
+      {2, 1}, {2, 3}, {2, 4}, {2, 10},
+      {3, 0}, {3, 1}, {3, 2}, {3, 4}, {3, 5},
+      {4, 2}, {4, 3}, {4, 5}, {4, 7}, {4, 8}, {4, 10},
+      {5, 0}, {5, 3}, {5, 4}, {5, 6}, {5, 7},
+      {6, 5}, {6, 7},
+      {7, 4}, {7, 5}, {7, 6}, {7, 8},
+      {8, 4}, {8, 7}, {8, 9}, {8, 10}, {8, 11},
+      {9, 8}, {9, 11},
+      {10, 2}, {10, 4}, {10, 8}, {10, 11},
+      {11, 8}, {11, 9}, {11, 10}
+    };
+
+    UnweightedGraph<String> graph = new UnweightedGraph<>(vertices, edges);
+    UnweightedGraph<String>.SearchTree dfs = graph.dfsNonRecurs(graph.getIndex("Chicago")); 
+
+    java.util.List<Integer> searchOrders = dfs.getSearchOrder();
+    
+    System.out.println(dfs.getNumberOfVerticesFound() + " vertices are searched in this DFS order:");
+    for (int i = 0; i < searchOrders.size(); i++) {
+      System.out.print(graph.getVertex(searchOrders.get(i)) + " ");
+    }
+    System.out.println('\n');
+
+    for (int i = 0; i < searchOrders.size(); i++)
+      if (dfs.getParent(i) != -1)
+        System.out.println("parent of " + graph.getVertex(i) + " is " + graph.getVertex(dfs.getParent(i)));
+  }
+}
